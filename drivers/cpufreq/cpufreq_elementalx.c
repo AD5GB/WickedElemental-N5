677aeadc8df27c5358f84540cd9b33addb2e22ea
Randy <randy.ducharme.@quikq.com>
2015-04-28 14:21:01 -0500

    Port new govs. Nuke unused funcs. Add static qualifiers.
    Signed-off-by: Randy <randy.ducharme.@quikq.com>

--- drivers/cpufreq/cpufreq_elementalx.c
+++ drivers/cpufreq/cpufreq_elementalx.c
@@ -722,7 +722,7 @@ void set_two_phase_freq_by_cpu ( int cpu_nr, int cpufreq){
 	two_phase_freq_array[cpu_nr-1] = cpufreq;
 }
 
-int input_event_boosted(void)
+static int input_event_boosted(void)
 {
 	unsigned long flags;
 
@@ -1177,10 +1177,6 @@ static struct input_handler dbs_input_handler = {
 	.id_table	= dbs_ids,
 };
 
-
-void set_input_event_min_freq_by_cpu ( int cpu_nr, int cpufreq){
-	input_event_min_freq_array[cpu_nr-1] = cpufreq;
-}
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
 {
